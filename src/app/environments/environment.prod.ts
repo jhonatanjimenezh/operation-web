e4e03dc61823c393eb13f7c556bc63e934b203b3
Jhonatan Jimenez H <jhonatanjimenezh@gmail.com>
2023-12-02 05:18:50 -0500

    Created operation web documentation

--- src/app/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  basePath: 'http://localhost:8080',
-  maxIntegerSolverPath: '/max-integer-solver/api',
-  operationService: '/v1/operation'
-};
-
